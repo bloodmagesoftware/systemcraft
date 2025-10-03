99d32a3cd4114fd10ab49f8efe2595460e189b85
Frank Mayer <gh@frankmayer.dev>
2025-10-03 02:08:17 +0200

    fix url

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://systemcraft.com",
+	site: "https://systemcraft.dev",
 
 	integrations: [
 		expressiveCode(),
